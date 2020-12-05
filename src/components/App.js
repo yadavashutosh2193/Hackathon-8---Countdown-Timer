b042648298cc24c8debdd279ecd68ab8b5851ae3
Win-10 <yadavashutosh2193@gmail.com>
2020-12-05 18:26:42 +0530

    done first edit

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,37 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
-
+const [counter, setCounter] = useState(0);
+  const keyDownfunction = (keypress)=>{
+    setCounter(0);
+    let inputvalue = Math.floor(keypress.target.value);
+    if(isNaN(inputvalue)){
+      setCounter(0);
+    }else if(keypress.keyCode === 13){
+      setCounter(inputvalue);  
+     
+    }
+  } 
+ useEffect(()=>{
+  const intervalId = setInterval(counterfn, 1000);
+  function counterfn(){
+    if(counter == 0){
+      clearInterval(intervalId);
+    }
+    else{
+      setCounter(counter - 1);
+    }
+  }
+ });
+  
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={keyDownfunction} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{counter}</div>
     </div>
   )
 }
